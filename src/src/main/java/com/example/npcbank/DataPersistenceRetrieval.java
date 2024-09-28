18a6e0a062223470f99a50292adca39d59f027db
Muhsen AbuMuhsen <118202203+Muhsen01@users.noreply.github.com>
2024-09-28 16:42:43 -0500

    Delete src directory

--- src/src/main/java/com/example/npcbank/DataPersistenceRetrieval.java
+++
@@ -1,9 +0,0 @@
-package com.example.npcbank;
-import java.util.ArrayList;
-
-public interface DataPersistenceRetrieval
-{
-    void persist(ArrayList<Object> list, String fileName);
-    ArrayList<Object> retrieve(String fileName);
-
-}
--- src/src/main/java/com/example/npcbank/models/TransactionType.java
+++
@@ -1,6 +0,0 @@
-package com.example.npcbank.models;
-
-public enum TransactionType
-{
-    DEPOSIT, WITHDRAWAL, TRANSFER
-}
